284b60ff2cabac7b34236be0b4e6f9ee0cc80132
Lovisa <lovisajernberg@gmail.com>
2018-11-12 17:45:29 +0100

    best movies ever

--- LovisaCheckpointNummer4/Program.cs
+++ LovisaCheckpointNummer4/Program.cs
@@ -7,6 +7,7 @@ namespace LovisaCheckpointNummer4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("best movies ever!");
         }
     }
 }
